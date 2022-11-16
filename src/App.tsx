32a92ca84dcbdbd6ca2f0b1aaeb3a755f6e8d20d
chengeheng <chengeheng@cowarobot.com>
2022-11-16 17:04:21 +0800

    chore: init program

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import "./App.css";
+
+import Map from "./map/index";
+
+// const data = {
+//   point: [
+//     {
+//       type: "Point",
+//       coordinates: [116.357126, 39.93574],
+//     },
+//     {
+//       type: "Point",
+//       coordinates: [116.400819, 39.918034],
+//     },
+//   ],
+//   path: [],
+// };
+
+function App() {
+  return (
+    <div className="App">
+      <Map />
+    </div>
+  );
+}
+
+export default App;
