a28e31a81f24e7e638e59edeb556156ea12d0a18
chengeheng <chengeheng@cowarobot.com>
2023-01-03 10:04:02 +0800

    feat: 补充label图层导出

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import Map from "./map/Map";
 import PointLayer from "./map/PointLayer";
 import PolygonLayer from "./map/PolygonLayer";
 import PolyLineLayer from "./map/PolyLineLayer";
+import LabelLayer from "./map/LabelLayer";
 
-export { Map as default, PointLayer, PolygonLayer, PolyLineLayer };
+export { Map as default, PointLayer, PolygonLayer, PolyLineLayer, LabelLayer };
