d18138fefb2ad837659ce654ffcb25e5d712e107
chengeheng <chengeheng@cowarobot.com>
2023-03-01 16:44:40 +0800

    feat: 修改版本号，发布v1.1.3版本

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,4 +2,5 @@ import Map from "./map/Map";
 import PointLayer from "./map/PointLayer";
 import PolygonLayer from "./map/PolygonLayer";
 import PolyLineLayer from "./map/PolyLineLayer";
-export { Map as default, PointLayer, PolygonLayer, PolyLineLayer };
+import LabelLayer from "./map/LabelLayer";
+export { Map as default, PointLayer, PolygonLayer, PolyLineLayer, LabelLayer };
